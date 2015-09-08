1b121d77fa2c9d6b7e19c2985e4af094f385ae6f
Hafiz Ismail <sgk.sprm@gmail.com>
2015-09-08 18:16:15 +0800

    Initial commit (forked from github.com/relayjs/relay-starter-kit )

---
+++ js/components/App.js
@@ -0,0 +1,33 @@
+import 'babel/polyfill';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Widget list</h1>
+        <ul>
+          {this.props.viewer.widgets.edges.map(edge =>
+            <li>{edge.node.name} (ID: {edge.node.id})</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Relay.createContainer(App, {
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on User {
+        widgets(first: 10) {
+          edges {
+            node {
+              id,
+              name,
+            },
+          },
+        },
+      }
+    `,
+  },
+});
---
+++ js/routes/AppHomeRoute.js
@@ -0,0 +1,10 @@
+export default class extends Relay.Route {
+  static queries = {
+    viewer: () => Relay.QL`
+      query {
+        viewer
+      }
+    `,
+  };
+  static routeName = 'AppHomeRoute';
+}
